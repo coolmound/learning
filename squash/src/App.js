84878e9a27cfade4f39d2a5d804f4b368ef82860
Julia_Sanchez <yulya.sanches@gmail.com>
2024-10-31 14:45:39 +0200

    .

--- squash/src/App.js
+++ squash/src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
+import {ReactComponent as Logo } from './svg/image.svg';
+// import logo from './svg/image.svg'
+
 import './App.css';
+const myComp = <div>vjhkvhrith</div>
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1 className='title'>You can do it!</h1>
-        <p className='text'>Although web design has a fairly recent history, it can be linked to other areas such as graphic design, user experience, and multimedia arts, but is more aptly seen from a technological standpoint.</p>
-        <button className='button'>JUST START</button>
+      <div className='general'>
+        <div className='leftside'>
+          <h1 className='title'>You can do it!</h1>
+          <p className='text'>Although web design has a fairly recent history, it can be linked to other areas such as graphic design, user experience, and multimedia arts, but is more aptly seen from a technological standpoint.</p>
+          <button className='button' onMouseEnter={()=>{alert('ok')}}>JUST START</button>
+        </div>
+        <div className='rightside'>
+          <Logo />
+          {myComp}
+          {/* <img src={logo} /> */}
+        </div>
       </div>
-      <div>
-        <img src='./svg/image.svg' alt='SVG Image' width={445} height={742}></img></div>
     </div>
   );
-}
 
+}
 export default App;
